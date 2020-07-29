789a7ee81d71703648ff8fafaaf8fb43cef387a6
Felipe Dolago <lagofelipe@gmail.com>
2020-07-29 23:02:46 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
   array.each_with_index do | item, index|
 
-puts index + 1.to_s + item
+puts index + 1.to_S + item
 #puts item
 
 
